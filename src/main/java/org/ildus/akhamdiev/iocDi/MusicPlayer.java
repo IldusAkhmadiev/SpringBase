c838ac26dc5b50c4e33e8c068908b9ea32ee9f7d
Ildus <goodgoo12@mail.ru>
2022-12-26 19:33:58 +0300

    DI 2

--- src/main/java/org/ildus/akhamdiev/iocDi/MusicPlayer.java
+++ src/main/java/org/ildus/akhamdiev/iocDi/MusicPlayer.java
@@ -2,6 +2,35 @@ package org.ildus.akhamdiev.iocDi;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
