195b68b8d416995172d541fddd025af5e4e64442
Ildus <goodgoo12@mail.ru>
2022-12-26 22:31:20 +0300

    scope testing

--- src/main/java/org/ildus/akhamdiev/iocDi/TestMusic.java
+++ src/main/java/org/ildus/akhamdiev/iocDi/TestMusic.java
@@ -1,5 +1,6 @@
 package org.ildus.akhamdiev.iocDi;
 
+import org.ildus.akhamdiev.base_core.BeanEx;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestMusic {
@@ -7,9 +8,13 @@ public class TestMusic {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer2",MusicPlayer.class);
-
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer2",MusicPlayer.class);
+        BeanEx testBean1 = context.getBean("testBean", BeanEx.class);
+        BeanEx testBean2 = context.getBean("testBean", BeanEx.class);
         musicPlayer.playMusic();
-
+        System.out.println("Singleton " + (musicPlayer == musicPlayer2) );
+        System.out.println(testBean1);
+        System.out.println(testBean2);
         context.close();
 
     }
