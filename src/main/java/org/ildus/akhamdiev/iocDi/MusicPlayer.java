9c2e01709e02680cc0a7a4a5c1b2bc9298302ac5
Ildus <goodgoo12@mail.ru>
2022-12-27 22:29:00 +0300

    add Scope Value

--- src/main/java/org/ildus/akhamdiev/iocDi/MusicPlayer.java
+++ src/main/java/org/ildus/akhamdiev/iocDi/MusicPlayer.java
@@ -2,15 +2,35 @@ package org.ildus.akhamdiev.iocDi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("musicPlayer2")
+@Scope("singleton")
 public class MusicPlayer {
     private Music music;
+    @Value("${musicPlayer.name}")
     private String name;
+    @Value("${musicPlayer.volume}")
     private int volume;
 
+//    @Autowired
+//    @Qualifier("popMusic")
+    private Music music2;
+//    @Autowired
+//    @Qualifier("popMusic")
+    private Music music3;
+
     public MusicPlayer() {
+        System.out.println("default");
+    }
+
+    @Autowired
+    public MusicPlayer(@Qualifier("popMusic") Music music2 ,@Qualifier("popMusic") Music music3) {
+        this.music2 = music2;
+        this.music3 = music3;
+        System.out.println(music2.getSong() + " " + music3.getSong());
     }
 
     public Music getMusic() {
@@ -45,5 +65,6 @@ public class MusicPlayer {
 
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+        System.out.println(name + " " + volume);
     }
 }
