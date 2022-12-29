17ead24f7f341fab6f41c87ccbea96ec6a3ae096
Ildus <goodgoo12@mail.ru>
2022-12-29 14:15:54 +0300

    add HelloController and html

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# SpringBase" 
 
-
+BaseJavaAnnoSpring,ListMusic,ListMusicAnno,
+xml and annotations branches refer to the base spring core
---
+++ src/main/java/org/ildus/akhamdiev/controller/HelloWorld.java
@@ -0,0 +1,37 @@
+package org.ildus.akhamdiev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWorld {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello/hello_world";
+    }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello/hello2";
+    }
+
+    @GetMapping("/hello3/")
+    public String hello3() {
+        return "hello/hello3";
+    }
+
+//    @RequestMapping(name = "/hello4/",method = RequestMethod.GET)
+//    public String hello4() {
+//        return "hello/hello4";
+//    }
+//
+//    @RequestMapping(name = "/hello5",method = RequestMethod.GET)
+//    public String hello5() {
+//        return "hello/hello5";
+//    }
+}
